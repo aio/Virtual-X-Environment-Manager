b760218c87fb4c391d5c1c4a85504f5a343aa1c5
aio <aio@aio-laptop.(none)>
2009-10-12 13:11:17 +0800

    Fixed minor bugs

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -1,8 +1,14 @@
 class Privilege < ActiveRecord::Base
+  validates_length_of :name, :within => 3..40
+  validates_presence_of :name, :resource_type, :resource_id
+  validates_uniqueness_of :name
+  
   @@resource_types = ['EVERYTHING', 
                       'VM',
                       'NODE',
-                      '',
-                      '',
+                      'STORAGE'
                      ]
+  def self.resource_types
+    @@resource_types
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,9 @@
 class Role < ActiveRecord::Base
+  validates_length_of :name, :within => 3..40
+  validates_length_of :description, :within => 0..80
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
   has_and_belongs_to_many :privileges, 
   :join_table => 'role_privilege_link', 
   :association_foreign_key => 'privilege_id', 
